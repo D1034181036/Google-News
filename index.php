dc94e922c62b606538c79e261d94582c688e9a9d
DESKTOP-57U3QJV\user01 <d1034181036@gm.lhu.edu.tw>
2024-07-15 17:18:19 +0800

    新增 "更多" 功能

--- index.php
+++ index.php
@@ -15,7 +15,20 @@
         .news-table a {color: #202020; text-decoration: none;}
         .news-table a:hover {text-decoration: underline;}
         .news-table td {padding: 8px; border: 1px solid;}
+        .hidden {display: none;}
     </style>
+    <script>
+        function toggleTd(button) {
+            var tr = button.parentElement.parentElement.nextElementSibling;
+            if (tr.classList.contains('hidden')) {
+                tr.classList.remove('hidden');
+                button.textContent = '收起';
+            } else {
+                tr.classList.add('hidden');
+                button.textContent = '更多';
+            }
+        }
+    </script>
 </head>
 <body>
     <table class="news-table">
@@ -23,6 +36,10 @@
             <?php foreach ($xmlElement->channel->item as $item): ?>
                 <tr>
                     <td><a href="<?= $item->link ?>" target="_blank"><?= $item->title ?></a></td>
+                    <td><button onclick="toggleTd(this)">更多</button></td>
+                </tr>
+                <tr class="hidden">
+                    <td><?= $item->description ?></td>
                 </tr>
             <?php endforeach; ?>
         </tbody>
